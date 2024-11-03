26efad31e4c42f3623bdaad9b5b596e47f139837
emino <emino@SHINUE>
2024-11-03 16:16:02 +0300

    Proje dosyası ekle.

---
+++ AutoShuddown.Designer.cs
@@ -0,0 +1,165 @@
+﻿// Bu proje MIT Lisansı altında lisanslanmıştır.
+// Daha fazla bilgi için LICENSE dosyasını kontrol edin.
+
+
+namespace AutoShuddown
+{
+    partial class AutoShuddown
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoShuddown));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownHeight = 100;
+            this.comboBox1.DropDownWidth = 25;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Location = new System.Drawing.Point(100, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(42, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Tag = "hour";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownHeight = 100;
+            this.comboBox2.DropDownWidth = 25;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.IntegralHeight = false;
+            this.comboBox2.Location = new System.Drawing.Point(176, 50);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(42, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.Tag = "minute";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Enabled = false;
+            this.textBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 13);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Lütfen bilgisayarın otomatik olarak kapatılacağı saati seçiniz.";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox2.Enabled = false;
+            this.textBox2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox2.Location = new System.Drawing.Point(12, 166);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(289, 13);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Kalan süre: ";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(212, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sayacı Durdur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox3.Enabled = false;
+            this.textBox3.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox3.Location = new System.Drawing.Point(226, 172);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(78, 13);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "By Shinıeeeeee";
+            // 
+            // AutoShuddown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(316, 191);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "AutoShuddown";
+            this.Text = "AutoShuddown";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ AutoShuddown.cs
@@ -0,0 +1,123 @@
+﻿// Bu proje MIT Lisansı altında lisanslanmıştır.
+// Daha fazla bilgi için LICENSE dosyasını kontrol edin.
+
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace AutoShuddown
+{
+    public partial class AutoShuddown : Form
+    {
+        private Timer countdownTimer;
+        private TimeSpan remainingTime;
+
+        public AutoShuddown()
+        {
+            InitializeComponent();
+            InitializeComboBoxes();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            countdownTimer = new Timer();
+            countdownTimer.Interval = 1000;
+            countdownTimer.Tick += CountdownTimer_Tick;
+        }
+
+        private void InitializeComboBoxes()
+        {
+            for (int hour = 0; hour < 24; hour++)
+            {
+                comboBox1.Items.Add(hour.ToString("D2"));
+            }
+
+            for (int minute = 0; minute < 60; minute++)
+            {
+                comboBox2.Items.Add(minute.ToString("D2"));
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateRemainingTimeDisplay();
+            CalculateRemainingTime();
+        }
+
+        private void CalculateRemainingTime()
+        {
+            if (comboBox1.SelectedItem != null && comboBox2.SelectedItem != null)
+            {
+                int selectedHour = int.Parse(comboBox1.SelectedItem.ToString());
+                int selectedMinute = int.Parse(comboBox2.SelectedItem.ToString());
+
+                DateTime currentTime = DateTime.Now;
+                DateTime selectedTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, selectedHour, selectedMinute, 0);
+
+                if (selectedTime < currentTime)
+                {
+                    selectedTime = selectedTime.AddDays(1);
+                }
+
+                remainingTime = selectedTime - currentTime;
+                countdownTimer.Start();
+            }
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            if (remainingTime.TotalSeconds > 0)
+            {
+                remainingTime = remainingTime.Subtract(TimeSpan.FromSeconds(1));
+                textBox2.Text = $"Kalan süre: {remainingTime:hh\\:mm\\:ss}";
+            }
+            else
+            {
+                countdownTimer.Stop();
+                ShutdownComputer();
+            }
+        }
+
+        private void ShutdownComputer()
+        {
+            Process.Start("shutdown", "/s /f /t 0");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void UpdateRemainingTimeDisplay()
+        {
+            if (comboBox1.SelectedItem != null && comboBox2.SelectedItem != null)
+            {
+                int selectedHour = int.Parse(comboBox1.SelectedItem.ToString());
+                int selectedMinute = int.Parse(comboBox2.SelectedItem.ToString());
+
+                DateTime currentTime = DateTime.Now;
+                DateTime selectedTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, selectedHour, selectedMinute, 0);
+
+                if (selectedTime < currentTime)
+                {
+                    selectedTime = selectedTime.AddDays(1);
+                }
+
+                TimeSpan tempRemainingTime = selectedTime - currentTime;
+                textBox2.Text = $"Kalan süre: {tempRemainingTime:hh\\:mm\\:ss}";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            countdownTimer.Stop(); // Geri sayımı durdurur
+        }
+    }
+}
